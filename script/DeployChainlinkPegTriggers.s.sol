e737da8fc86e77a94891746a4181c6dae29f00e6
chad-js <chadflemies@gmail.com>
2022-09-29 14:58:53 -0400

    parse json for peg chainlink trigger deploy script

--- script/DeployChainlinkPegTriggers.s.sol
+++ script/DeployChainlinkPegTriggers.s.sol
@@ -16,14 +16,14 @@ import "src/ChainlinkTriggerFactory.sol";
   * anvil --fork-url $OPTIMISM_RPC_URL
   *
   * # In a separate terminal, perform a dry run the script.
-  * forge script script/DeployChainlinkTriggers.s.sol \
-  *   --sig "run(string)" "deploy-chainlink-triggers-<test or production>" \
+  * forge script script/DeployChainlinkPegTriggers.s.sol \
+  *   --sig "run(string)" "deploy-chainlink-peg-triggers-<test or production>" \
   *   --rpc-url "http://127.0.0.1:8545" \
   *   -vvvv
   *
   * # Or, to broadcast transactions with etherscan verification.
-  * forge script script/DeployChainlinkTriggers.s.sol \
-  *   --sig "run(string)" "deploy-chainlink-triggers-<test or production>" \
+  * forge script script/DeployChainlinkPegTriggers.s.sol \
+  *   --sig "run(string)" "deploy-chainlink-peg-triggers-<test or production>" \
   *   --rpc-url "http://127.0.0.1:8545" \
   *   --private-key $OWNER_PRIVATE_KEY \
   *   --etherscan-api-key $ETHERSCAN_KEY \
@@ -32,7 +32,7 @@ import "src/ChainlinkTriggerFactory.sol";
   *   -vvvv
   * ```
  */
-contract DeployChainlinkTriggers is ScriptUtils {
+contract DeployChainlinkPegTriggers is ScriptUtils {
   using stdJson for string;
 
   // -----------------------------------
@@ -40,22 +40,22 @@ contract DeployChainlinkTriggers is ScriptUtils {
   // -----------------------------------
 
   struct ChainlinkMetadata {
+    // The amount of decimals used by the peg price and tracking oracle price.
+    uint8 decimals;
     // A human-readable description of the intent of the trigger.
     string description;
+    // The maximum amount of time we allow to elapse before the tracking oracle's price is deemed stale.
+    uint256 frequencyTolerance;
     // Logo uri that describes the trigger, as it should appear within the Cozy user interface.
     string logoURI;
     // The name of the trigger, as it should appear within the Cozy interface.
     string name;
+    // The peg price to compare the tracking oracle price to.
+    int256 pegPrice;
     // The maximum percent delta between oracle prices that is allowed, as a wad.
     uint256 priceTolerance;
-    // The maximum amount of time we allow to elapse before the tracking oracle's price is deemed stale.
-    uint256 trackingFrequencyTolerance;
     // The oracle we expect to diverge.
     AggregatorV3Interface trackingOracle;
-    // The maximum amount of time we allow to elapse before the truth oracle's price is deemed stale.
-    uint256 truthFrequencyTolerance;
-    // The canonical oracle, assumed to be correct.
-    AggregatorV3Interface truthOracle;
   }
 
   ChainlinkTriggerFactory factory;
@@ -81,26 +81,32 @@ contract DeployChainlinkTriggers is ScriptUtils {
   function _deployTrigger(ChainlinkMetadata memory _metadata) internal {
     console2.log("Deploying ChainlinkTrigger...");
     console2.log("    chainlinkTriggerFactory", address(factory));
-    console2.log("    truthOracle", address(_metadata.truthOracle));
+    console2.log("    pegPrice", uint256(_metadata.pegPrice));
+    console2.log("    decimals", uint256(_metadata.decimals));
     console2.log("    trackingOracle", address(_metadata.trackingOracle));
     console2.log("    priceTolerance", _metadata.priceTolerance);
-    console2.log("    truthFrequencyTolerance", _metadata.truthFrequencyTolerance);
-    console2.log("    trackingFrequencyTolerance", _metadata.trackingFrequencyTolerance);
+    console2.log("    frequencyTolerance", _metadata.frequencyTolerance);
     console2.log("    triggerName", _metadata.name);
     console2.log("    triggerDescription", _metadata.description);
     console2.log("    triggerLogoURI", _metadata.logoURI);
 
-    // Loosely validate oracle interfaces by ensuring `description()` doesn't revert.
-    _metadata.truthOracle.description();
+    // Loosely validate oracle interface by ensuring `description()` doesn't revert.
     _metadata.trackingOracle.description();
+
+    AggregatorV3Interface _truthOracleAddress = AggregatorV3Interface(
+      factory.computeFixedPriceAggregatorAddress(_metadata.pegPrice, _metadata.decimals)
+    );
 
     // Check to see if a trigger has already been deployed with the desired configs.
     address _availableTrigger = factory.findAvailableTrigger(
-      _metadata.truthOracle,
+      _truthOracleAddress,
       _metadata.trackingOracle,
       _metadata.priceTolerance,
-      _metadata.truthFrequencyTolerance,
-      _metadata.trackingFrequencyTolerance
+      // For the truth FixedPriceAggregator peg oracle, we use a frequency
+      // tolerance of 0 since it should always return block.timestamp as the
+      // updatedAt timestamp.
+      0,
+      _metadata.frequencyTolerance
     );
 
     if (_availableTrigger == address(0)) {
@@ -109,11 +115,11 @@ contract DeployChainlinkTriggers is ScriptUtils {
       vm.broadcast();
       _availableTrigger = address(
         factory.deployTrigger(
-          _metadata.truthOracle,
+          _metadata.pegPrice,
+          _metadata.decimals,
           _metadata.trackingOracle,
           _metadata.priceTolerance,
-          _metadata.truthFrequencyTolerance,
-          _metadata.trackingFrequencyTolerance,
+          _metadata.frequencyTolerance,
           IChainlinkTriggerFactory.TriggerMetadata(
             _metadata.name,
             _metadata.description,
